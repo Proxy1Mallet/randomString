0723d688fcbf2d57b6b1555b6ced54c6c867b0d1
Proxy Mallet <qq84063@gmail.com>
2022-10-23 00:04:19 +0500

    Create randomString.py

---
+++ randomString.py
@@ -0,0 +1,11 @@
+randint = __import__('random').randint
+choice = __import__('random').choice
+
+def RandomString(a, abcCase, intCase) -> str:
+    for _ in range(int(input('input >>> '))):
+        a += choice([str(choice(abcCase)).upper(), str(choice(abcCase)), choice(intCase)])
+    return a
+
+print(RandomString(a = '',
+                   abcCase = 'abcdefghijklmnopqrstuvwxyz',
+                   intCase = '123456789'))
