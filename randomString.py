0baf2757bdf84b9f95bdc564a3607cc4c41e989b
Proxy Mallet <qq84063@gmail.com>
2022-11-17 14:51:02 +0500

    Update randomString.py

--- randomString.py
+++ randomString.py
@@ -1,10 +1,13 @@
 choice = __import__('random').choice
 
-def RandomString(a, abcCase, intCase) -> str:
+def RandomString(a, lang : str, intCase : str) -> str:
+    if lang == 'RU': abcCase = 'абвгдеёжзийклмопрстуфхцчъьэюя'
+    else: abcCase = 'abcdefghijklmnopqrstuvwzyz'
+
     for _ in range(int(input('input >>> '))):
         a += choice([str(choice(abcCase)).upper(), str(choice(abcCase)), choice(intCase)])
     return a
 
 print(RandomString(a = '',
-                   abcCase = 'abcdefghijklmnopqrstuvwxyz',
+                   lang = 'RU',
                    intCase = '123456789'))
