254565f15169b1eb39ddf5bca978ffa3f64deb9c
Proxy Mallet <qq84063@gmail.com>
2022-12-05 18:46:38 +0500

    Update randomString.py

--- randomString.py
+++ randomString.py
@@ -1,10 +1,8 @@
-from random import choices
-
 def RandomString(lang : str, intCase) -> str:
     if lang == 'RU': abcCase = 'абвгдеёжзсийклмнопрстуфхччъьэюя'
     else: abcCase = 'abcdefghijklmnopqrstuvwxyz'
 
-    a = ''.join(choices(f'{abcCase} {intCase}', k=462))
+    a = ''.join(__import__('random').choices(f'{abcCase} {intCase}', k=462))
     return a
 
 print(RandomString(lang='RU', intCase='123456789'))
